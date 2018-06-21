bb2c648461a675a0c5b315155e6625a7c531a2c9
SyllabusGames <33643978+SyllabusGames@users.noreply.github.com>
2018-06-21 15:56:00 -0400

    Add files via upload

--- SineRider/js/Sledder.js
+++ SineRider/js/Sledder.js
@@ -41,31 +41,20 @@ function resetSledder(){
 	vx = 0;
 	vy = 0;
 	rotation = 0;
-	screenFollowSledder();//		reset screen position
+	if(camLocked)
+		screenFollowSledder();//		reset screen position
 	//		reset screen too. This should probably be moved somewhere else.
 }
 
 	//		-----------------------------------------------------------------------		[   UPDATE   ]		-----------------------------------------------------------------------
 function moveSledder(){
 	if(simulating){
-		//		sledder kinematics
-		//		while velocity and acceleration are calculated as raw math, when converting to world space, changes in position must be divided by screenScale
-	//	ctx.fillText("Vy = " + Math.round(vy*10).toString(),10,150);
-	//	ctx.fillText("Vx = " + Math.round(vx*10).toString(),10,50);
-		
-		apx += vx*dt*0.15;
+		apx += vx*dt*0.15;//		absolute position (in meters)
 		apy += vy*dt*0.15;
 
-		spx = (apx)*screenScale - screenx*screenScale;
-		spy = -(apy)*screenScale + screeny*screenScale;
-		//		test curvV with y=-x*2+(x/10-5)^2+sin(x/5+t)*15+10-x-5
-		//						y=sin(x/3+t*2)*3-x/2
-		//						y=sin(t*4)*3-x/2
+		spx = (apx)*screenScale - screenx*screenScale;//		screen position (in pixels)
+		spy = -(apy)*screenScale + screeny*screenScale;//		y is flipped since the top of the screen is 0
 
-	//	console.log("vy="+vy+" vx="+vx+"vMag="+Math.sqrt(vx*vx+vy*vy));
-		//		friction
-		//vx *= 0.9999;
-		//vy *= 0.9999;
 		drawSledder();
 		//		apply gravity
 		vy += ay*dt;
@@ -81,7 +70,7 @@ function moveSledder(){
 	}
 
 	//		----------------------------------------------------		[   Move/Scale Screen   ]		----------------------------------------------------
-	if(simulating){
+	if(simulating && camLocked){
 		screenFollowSledder();
 	}
 }
@@ -148,91 +137,35 @@ function drawSledder(){
 		apy = ftmp;//		snap to surface of graph so you do not pass through it.
 		tempZ = apz;//		set the Z coordiante to the sled's Z coordiante
 
-		//		velocity is set by the derrivative of the graph at the contact point
-		//vy = (equation((spx+1)/screenScale) - ftmp)/screenScale*40000*dt;
 		//		get the tangent unit vector
 		dy = (equation(apx+0.1) - ftmp);//		get vertical displacment 0.1 meter to the right. dx = 1.
 		dx = 1/Math.sqrt(0.01 + dy*dy);//		divide dx by the length of the tangent line formed by [0.1 , dy]
 		dy *= dx;//		multiply dy by dx so [dx , dy] will be 1 meter long after the next opperation (unit tangent vector)
 		dx *= 0.1;//	dx was a multiplier for dy, now turn it back into dx (the x component of the tangent unit vector)
 		dtmp = vx*dx + vy*dy;//		Dot Product of velocity and slope's tangent.		(Ammount of velocity along the graph) Dot = a.x*b.x + a.y*b.y
-		/*
-		//		Graph's tangent
-		ctx.beginPath();
-		ctx.moveTo(spx , spy);
-		ctx.lineTo(spx+dx*50 , spy-dy*50);
-		ctx.stroke();
-
-		//		slope normal vector
-		ctx.strokeStyle="#00FF00";
-		ctx.beginPath();
-		ctx.moveTo(spx , spy);
-		ctx.lineTo(spx-dy*50 , spy-dx*50);
-		ctx.stroke();
-
 		
-		//		velocity vector
-		ctx.strokeStyle="#FF0000";
-		ctx.beginPath();
-		ctx.moveTo(spx , spy);
-		ctx.lineTo(spx+vx*5 , spy-vy*5);
-		ctx.stroke();
-
-		
-		//		velocity DOT slope normal
-		ctx.strokeStyle="#0000A0";
-		ctx.beginPath();
-		ctx.moveTo(spx , spy);
-		ctx.lineTo(spx , spy-(-dy*vx + -dx*-vy));
-		ctx.stroke();
-	//	console.log((-dy*vx + -dx*-vy));//		if dot product of slope normal and velocity is negative, sled is trying to go through the line
-	*/
-
-		//									[[[			ENABLING THIS SWITCHES LINE MOMENUM TRANSFER OFF			]]]
 		if((-dy*vx + -dx*-vy) < 0){//		if dot product of slope normal and velocity is negative, sled is trying to go through the line
 			vx = dx*dtmp;//		new velocity along x set by amount of original velocity that was in the direction tangent to the equation line
 			vy = dy*dtmp;
 		}
-
-
-
-
-
-
 
 	//		----------------------------------------------------		[   Set Velocity by line movement   ]		----------------------------------------------------
 		//		since the graph can only change allong Y so the change in Y is used as the full displacment vector.
 		frameTime -= dt*2;//		I am not sure why the *2 is nessisary but it just is
 	//	dtmp = apz;
 	//	apz = lastApz;//		use last frame's z value so if it changed, that will be incorporated into the sled's movement
-		//		test (rotation)				-((x-22)*t*0.3-2)/5-5
-		//		test (slightly slopped trampoline)	 -x/100+sin(t*1.5+1.5)*7
-		//		test (neat)			-x/3-5+sin(-t+x/2)*2+((1+sin(t))*x/10-2)^2+sin(t)*4
 		
-		dxdt = (ftmp - equation(apx));//		change in curve's y since 2 frames ago
-		
-		//		(y pos) - (y pos 1 frame ago)
-		dydt = (ftmp - equation(apx));//		dydx is just the y component of the velocity vector.
+		dxdt = ftmp - equation(apx);//		change in curve's y position under sled since 2 frames ago
 		
 		//		tmspx and tmspy are the x and y components of the velocity of the graph at [spx,spy]
 		//		this will push the sled more left or right when the slope is steeper and more vertically when the slope is more level
 		tmspx = dy * dxdt;
-		tmspy = dx * dydt;
-	//	console.log(ftmp + " - " + equation(apx) + " - " + apx + " == " + tmspy + " vy= " + vy);
+		tmspy = dx * dxdt;
 
-		if(tmspy > 0){//		The sled cannot be pulled by the ground, only pushed.
+		if(tmspy > 0){//		Curve is rising (the sled cannot be pulled by the ground, only pushed)
 			vy += tmspy*0.5/dt;//		divide by 2 to compensate for multiplying the change in time by 2
 			vx -= tmspx*0.5/dt;
-		//	console.log("push" + vy);
 		}
-		/*
-		if(vy > 0.5*tmspy/dt){
-			console.log("bump" + vy + " - " + 0.5*tmspy/dt);
-	//		vy = 10*0.5*tmspy/dt;
-		}
-		*/
-		//		test			min(-5,sin(t+x/4)*10)*max(5,sin(t+x/4)*10)
-		//		square wave		max(-3,min(3,sin(-t+x/4)*100))-5
 		frameTime += dt*2;
 	//	apz = dtmp;//		restore the sled's z position
 
@@ -249,13 +182,6 @@ function drawSledder(){
 		av = 0;//		set angular velocity to 0 as it will be set below if the sled is not level on the track
 	}
 		
-	//	ctx.fillText("Screen X = " + Math.round(screenx).toString(),10,50);
-	//	ctx.fillText("Screen Y = " + Math.round(screeny).toString(),10,120);
-	//	ctx.fillText("spx = " + Math.round(spx).toString(),10,180);
-	//	ctx.fillText("spy = " + Math.round(spy).toString(),10,240);
-	//	ctx.fillText("tmspy = " + Math.round(tmspy*10).toString(),10,280);
-	//	ctx.fillText("dx = " + Math.round(dx*10).toString(),10,330);
-	//	ctx.fillText("dy = " + Math.round(dy*10).toString(),10,400);
 	//		----------------------------------------------------		[   Set Rotation   ]		----------------------------------------------------
 	if((equation(apx + boxx) <  apy + boxy)		||		(equation(apx - boxx) <  apy - boxy)){//		one of the lower corners is below the line
 	//if((equation((spx + boxx - boxy/4)/screenScale) <  spy + boxx - boxy/4)		||		(equation((spx - boxx - boxy/4)/screenScale) <  spy - boxx - boxy/4)){//		one of the lower corners is below the line
