535fbdb9ab5945fb3298853bee13409c7897afbc
SyllabusGames <33643978+SyllabusGames@users.noreply.github.com>
2018-09-19 18:18:10 -0400

    Add files via upload

---
+++ EduGameListJs/ListSort.js
@@ -0,0 +1,113 @@
+﻿//	0			1			2		3										4		5	6	7		8		9			10									11	12
+//["Edu", "Programming", "No", "Basic Programming (Drag 'n drop)", "Run Marco!", "4+", "1", "0", "Web", "RunMarko", "https://www.brainpop.com/games/runmarco/",,],
+
+function sortList(a, b) {
+//		store the variable used to sort the list in slot 12 so the list can be grouped by that variable
+	if(sortVar[0] < 4){//		only type, subject, and rating get grouped. Everything else gets set to be the same group.
+		a[11] = a[sortVar[0]];
+		b[11] = b[sortVar[0]];
+	}else{
+		a[11] = 0;
+		b[11] = 0;
+	}
+	for (k = 0; k < 3; k++) {//		loop through the 3 variables to sort by
+		//		every function called sets itmp internally
+		switch (sortVar[k]) {
+			case 0://	type
+				itmp = sortByType(a, b);
+				break;
+			case 1://	subject
+				itmp = a[1].localeCompare(b[1]);
+				break;
+			case 2://	rating
+				itmp = sortByRating(a, b);
+				break;
+			case 4://	name
+				itmp = a[4].localeCompare(b[4]);
+				break;
+			case 5://	age
+				itmp = a[5].localeCompare(b[5]);
+				break;
+			case 6://	length
+				itmp = a[6].localeCompare(b[6]);
+				break;
+			case 7://	price
+				itmp = a[7].localeCompare(b[7]);
+				break;
+			case 8://	format
+				itmp = a[8].localeCompare(b[8]);
+				break;
+		}
+		if (itmp != 0)
+			return itmp;//		if the compared variable was not the same for both, retun which was greater
+	}//		if all three sort variables are the same for a game, sort by Name
+	return a[4].localeCompare(b[4]);//		sort by name
+}
+
+function sortByType(a, b) {
+	itmp = 0;
+	switch (a[0]) {//		No just leaves itmp at 0
+		case "Edu":
+			itmp = -3;
+			break;
+		case "Sim":
+			itmp = -2;
+			break;
+		case "HW":
+			itmp = -1;
+			break;
+		case "Issue":
+			itmp = 1;
+			break;
+	}
+	switch (b[0]) {
+		case "Edu":
+			itmp += 3;
+			break;
+		case "Sin":
+			itmp += 2;
+			break;
+		case "HW":
+			itmp += 1;
+			break;
+		case "Issue":
+			itmp += -1;
+			break;
+	}
+	return itmp;
+}
+
+function sortByRating(a, b) {
+	switch (a[2]) {//		No just leaves itmp at 0
+		case "YES!":
+			itmp = -3;
+			break;
+		case "Yes":
+			itmp = -2;
+			break;
+		case "Maybe":
+			itmp = -1;
+			break;
+		case "No":
+			itmp = 0;
+			break;
+		default:
+			itmp = 1;
+			break;
+	}
+	switch (b[2]) {
+		case "YES!":
+			itmp += 3;
+			break;
+		case "Yes":
+			itmp += 2;
+			break;
+		case "Maybe":
+			itmp += 1;
+			break;
+		default:
+			itmp += -1;
+			break;
+	}
+	return itmp;
+}
--- SmoothScrolling.js
+++ SmoothScrolling.js
@@ -8,24 +8,34 @@ var fadeIn = 0;
 
 document.addEventListener('wheel', function (e){
 	e.preventDefault();
-	dy -= Math.sign(e.deltaY)*250;
+	dy -= Math.sign(e.deltaY)*240;
 	direction = Math.sign(dy);
 	if(!scrolling){//		not currently scrolling
 		fadeIn = 0;//		slow start for scrolling
 		smoothScroll();
 	}
 });
+
 function smoothScroll(){
-	//		change in screen position each frame calculated by y=log(abs(x/10)+1)*4+abs(x/25)+0.1 where x = differential between destination and current scroll position
-	dydt = -Math.sign(dy) * (Math.log(Math.abs(dy / 10) + 1) * 4 + Math.abs(dy/25)+ 0.1);
+	//		change in screen position each frame calculated by equation below
+
+//	----	S curve equation y=5/(1+3^(3-abs(x/20)))+abs(x/15)
+	dydt = -Math.sign(dy) * (4/(1 + Math.pow(3 , 2 - Math.abs(dy/20))) + Math.abs(dy/10));
+
+//	----	Logrithmic equation for steep falloff at end of scroll y=log(abs(x/10)+1)*4+abs(x/25)+0.1 where x = differential between destination and current scroll position
+//	dydt = -Math.sign(dy) * (Math.log(Math.abs(dy / 10) + 1) * 4 + Math.abs(dy/25)+ 0.1);
+
+//	console.log(dy + " - " + dydt);
+//		timed fade in. At +0.07 a frame and 60fps, it takes 1/4 of a second to reach full scroll speed
 	fadeIn = Math.min(fadeIn + 0.07, 1);
 	dydt *= fadeIn;//		slow ramp up when scrolling first starts
-	dy = Math.round(dy + dydt);
+	//		floor dy so it is  
+	dy += dydt;
 	if(direction != Math.sign(dy)){//		you have scrolled too far and are about to reverse direction
 		dy = 0;
 	}
 	window.scrollBy(0, dydt);
-	scrolling = !(dy == 0);
+	scrolling = Math.abs(dy) > 0.5;//		screen is not within 0.5 pixels of its end point. Keep scrolling the screen.
 	if(scrolling){//		still scrolling
 		window.requestAnimationFrame(smoothScroll);
 	}
